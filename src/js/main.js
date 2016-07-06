e2ff4844f9cb209de5887c360756c34a3ee15589
yoichi kobayashi <info@tplh.net>
2016-07-06 23:14:43 +0900

    [remove] unnecessary files.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,4 @@
-import initIndex from './init/index.js'
-
-const { pathname } = window.location;
-
 const init = () => {
-  switch (pathname.replace('index.html', '')) {
-    case '/':
-      initIndex();
-      break;
-    default:
-  }
+
 }
 init();
